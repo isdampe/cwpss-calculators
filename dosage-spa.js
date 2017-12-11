006db74746e0f5dea90dfce22bd951683ddb0f72
Richard Denton <isdampe@gmail.com>
2017-12-11 14:05:14 +1100

    Add spa dosage calculator

--- dosage-spa.js
+++ dosage-spa.js
@@ -1,12 +1,12 @@
-class DosagePoolCalculator {
+class DosageSpaCalculator {
 
 	/**
-	 * @param  {Number} poolSizeLitres The number of litres for the instance.
+	 * @param  {Number} spaSizeLitres The number of litres for the instance.
 	 * @return {void}
 	 */
-	constructor(poolSizeLitres) {
-		this.volume = poolSizeLitres;
-		this.volumeFactor = (poolSizeLitres / 10000);
+	constructor(spaSizeLitres) {
+		this.volume = spaSizeLitres;
+		this.volumeFactor = (spaSizeLitres / 1000);
 	}
 
 	/**
@@ -16,7 +16,7 @@ class DosagePoolCalculator {
 	 */
 	computeAlkalinity(input) {
 
-		const alkHigh = 125, alkLow = 80, alkPpm = 10, alkDose = 200;
+		const alkHigh = 150, alkLow = 120, alkPpm = 10, alkDose = 18;
 
 		var res = {
 			tips: [],
@@ -25,18 +25,20 @@ class DosagePoolCalculator {
 
 		var tipOne = "";
 		if (input >= alkLow && input <= alkHigh) {
-			res.tips.push("Congratulations your Pool Alkalinity is within normal range. Please check your pH readings.");
+			res.tips.push("Congratulations your Spa is within normal range. Please check your pH readings.");
 		} else if (input < alkLow) {
 
 			//Compute res.value.
 			var diff = alkHigh - input;
 			res.value = ((diff / alkPpm) * alkDose) * this.volumeFactor;
-			
-			res.tips.push("You will need to INCREASE the ALKALINITY level by using CRYSTAL WATERS pH BUFFER. Increasing total Alkalinity may efvfect the pH reading, recheck pH after Alkalinity is correct.");
-			res.tips.push("Desolve pH Buffer in a bucket, then distribute over pool surface with filter running for 4-5 hours.");
+
+			res.tips.push("You will need to INCREASE the ALKALINITY level by using [CRYSTAL WATERS SPA PH AND ALKALINITY ENHANCER]");
+			if ( res.value > (this.volumeFactor * 40) )
+				res.tips.push("Maximium dosage per time is 40 grams per 1,000 ltrs then retest in half an hour. The use of [CRYSTAL WATERS SPA PH AND ALKALINITY ENHANCER] will also raise the pH level. Check pH after Total Alkalinity has been adjusted.");
 
 		} else if ( input > alkHigh ) {
-			res.tips.push("Your Alkalinity is higher than recommended. Use CRYSTAL WATERS POOL PH REDUCER to lower Alkalinity level and then retest.");
+			res.tips.push("Your Alkalinity is higher than recommended. Use [CRYSTAL WATERS SPA PH REDUCER] to lower Alkalinity level and then retest.");
+			res.tips.push("Proceed to Step Two and then retest for Alkalinity.");
 		}
 
 		return res;
@@ -58,18 +60,35 @@ class DosagePoolCalculator {
 		};
 
 		if (input >= pHLow && input <= pHHigh) {
-			res.tips.push("Congratulations your Pool is within normal range.");
+			res.tips.push("Congratulations your Spa is within normal range.");
 		} else if ( input < pHLow ) {
 			res.tips.push("Your pH is very low, please retest your Total Alkalinity level and adjust within the recommended range, this will also increase the pH level. Then retest pH level after 30 minutes.");
 		} else {
 
+			var oi = input;
 			if ( input > 8.2 )
 				input = 8.2;
 
 			var vi = (input * 10) - ((pHHigh * 10) -1);
-			res.value = (vi * ((pHHigh * 10) -1)) * this.volumeFactor;
-			res.tips.push("You will need to DECREASE the pH level by using CRYSTAL WATERS pH Down");
-			res.tips.push("Do not add more than 250 grams of pH DOWN per 10,000 litres of water at anyone time");
+			var bf = 1;
+			var scalar = 0;
+
+			for ( var i=0; i<vi; i++ ) {
+				if (bf === 1)
+					scalar += 7;
+				else
+					scalar += 8;
+				bf = bf * -1;
+			}
+
+			if ( bf === -1 )
+				scalar += 1;
+
+			res.tips.push("You will need to DECREASE the pH level by using [CRYSTAL WATERS SPA PH REDUCER]");
+			if (oi > 8.2) 
+				res.tips.push("After adding chemicals and running filter please retest Spa");
+
+			res.value = scalar * this.volumeFactor;
 
 		}
 
@@ -84,7 +103,7 @@ class DosagePoolCalculator {
 	 */
 	computeHardness(input) {
 
-		const hardnessLow = 200, hardnessHigh = 275, hardnessDose = 100, hardnessPpm = 10;
+		const hardnessLow = 80, hardnessHigh = 150, hardnessDose = 10, hardnessPpm = 10;
 
 		var res = {
 			tips: [],
@@ -92,14 +111,14 @@ class DosagePoolCalculator {
 		};
 
 		if ( input >= hardnessLow && input <= hardnessHigh ) {
-			res.tips.push("Congratulations your Pool Hardness is within normal range.");
+			res.tips.push("Congratulations your Spa is within normal range.");
 		} else if ( input > hardnessHigh ) {
 			res.tips.push("Your Calcium reading is above recommended range. Either dilute spa with fresh water if source water has a lower Calcium level. Alternativly for areas of high Calcium Hardness we reccommend the Total Alkalinity is set at the lower recommended level");
 		} else {
 			var diff = hardnessHigh - input;
 			res.value = ((diff / hardnessPpm) * hardnessDose) * this.volumeFactor;
-			res.tips.push("You will need to INCREASE the Calcium level with CRYSTAL WATERS HARDNESS INCREASER");
-			res.tips.push("Add one third of the total amount every 6 hours. Desolve Hardness in a bucket, then distribute over pool surface with filter running.");
+			res.tips.push("You will need to INCREASE the Calcium level with [CRYSTAL WATERS SPA CALCIUM ENHANCER]");
+			res.tips.push("After adding chemicals and running filter please retest Spa");
 		}
 
 		return res;
@@ -112,15 +131,15 @@ class DosagePoolCalculator {
 	 */
 	computeRecommendations() {
 
-		const sanitiser = 60, clarifer = 50, boost = 50;
+		const sanitiser = 60, clarifer = 60, boost = 50;
 		return {
-			'sanitiser': Math.round((this.volume / 10000) * sanitiser),
-			'clarifer': Math.round((this.volume / 10000) * clarifer),
-			'boost': Math.round((this.volume / 10000) * boost)
+			'sanitiser': Math.round((this.volume / 1000) * sanitiser),
+			'clarifer': Math.round((this.volume / 1000) * clarifer),
+			'boost': Math.round((this.volume / 1000) * boost)
 		};
 
 	}
 
 }
 
-module.exports = DosagePoolCalculator;
+module.exports = DosageSpaCalculator;
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const CostingCalculator = require('./costing')
-const DosagePoolCalculator = require('./dosage-pool')
+const CostingCalculator = require('./costing');
+const DosagePoolCalculator = require('./dosage-pool');
+const DosageSpaCalculator = require('./dosage-spa');
 
 var cc = new CostingCalculator(1500);
 console.log('Costing:');
@@ -17,3 +18,17 @@ console.log(dc.computeHardness(555));
 
 console.log('Recommendations:');
 console.log(dc.computeRecommendations());
+
+var sc = new DosageSpaCalculator(2500);
+
+console.log('Spa Alkalinity:');
+console.log(sc.computeAlkalinity(155));
+
+console.log('pH:');
+console.log(sc.computePH(7.9));	
+
+console.log('Hardness:');
+console.log(sc.computeHardness(165));
+
+console.log('Recommendations');
+console.log(sc.computeRecommendations());
